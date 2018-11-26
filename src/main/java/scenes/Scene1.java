7b2cd9d1ae2fc2f3702a3ac4cda070e23371c20f
alexandr <fart82@ukr.net>
2018-11-26 20:58:33 +0200

    set style

--- src/main/java/scenes/Scene1.java
+++ src/main/java/scenes/Scene1.java
@@ -5,23 +5,27 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 class Scene1 {
-
     void scene1(Stage stage) {
         stage.setTitle("Hotel Booking");
-        VBox rood = new VBox();
-        rood.setSpacing(10);
+        VBox root = new VBox();
+        root.setSpacing(10);
         Label label = new Label("Scene 1");
-        Scene scene1 = new Scene(rood, 800, 600);
+        Scene scene = new Scene(root, 800, 600);
+
+        //set style
+        BackgroundImage myBI= new BackgroundImage(new Image("8M5A7719 edited.jpg",800,600,false,true),
+                BackgroundRepeat.ROUND, BackgroundRepeat.ROUND, BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
+        root.setBackground(new Background(myBI));
 
         //reading from file
         ArrayList<String> arrayList = new ArrayList<>();
@@ -45,62 +49,56 @@ class Scene1 {
         Button buttonScene = new Button("To scene 2");
         buttonsScene.getChildren().addAll(buttonScene);
 
-        //set separators lines
-        Separator separator1 = new Separator();
-        separator1.setMaxWidth(500);
-        Separator separator2 = new Separator();
-        separator2.setMaxWidth(500);
-
         //set text fields
-        VBox roodText = new VBox();
-        roodText.setAlignment(Pos.CENTER);
+        VBox textFields = new VBox();
+        textFields.setAlignment(Pos.CENTER);
         TextField fieldName = new TextField();
         fieldName.setMaxWidth(500);
         fieldName.setPromptText("Enter name");
         TextField fieldEmail = new TextField();
         fieldEmail.setMaxWidth(500);
         fieldEmail.setPromptText("Enter email");
-        roodText.getChildren().addAll(label, list, fieldName, fieldEmail, separator1);
+        textFields.getChildren().addAll(label, list, fieldName, fieldEmail);
 
         //set date fields
-        HBox hBox = new HBox();
-        hBox.setAlignment(Pos.CENTER);
-        hBox.setSpacing(100);
+        HBox dateFields = new HBox();
+        dateFields.setAlignment(Pos.CENTER);
+        dateFields.setSpacing(100);
         DatePicker datePicker1 = new DatePicker();
         datePicker1.setPromptText("Enter date from");
         DatePicker datePicker2 = new DatePicker();
         datePicker2.setPromptText("Enter date till");
-        hBox.getChildren().addAll(datePicker1, datePicker2);
+        dateFields.getChildren().addAll(datePicker1, datePicker2);
 
         //set buttons
         HBox buttons = new HBox();
         buttons.setAlignment(Pos.CENTER);
         buttons.setSpacing(20);
-        Button button1 = new Button("set");
+        Button buttonSet = new Button("set");
         Button buttonClear = new Button("clear");
-        buttons.getChildren().addAll(button1, buttonClear);
+        buttons.getChildren().addAll(buttonSet, buttonClear);
 
         //set radio buttons
-        RadioButton radioClear = new RadioButton("Add clearing 1$");
-        RadioButton radioBreakfasr = new RadioButton("Add breakfast 2$");
         HBox radioButtons = new HBox();
-        radioButtons.getChildren().addAll(radioClear, radioBreakfasr);
+        RadioButton radioClear = new RadioButton("Add clearing 1$");
+        RadioButton radioBreakfast = new RadioButton("Add breakfast 2$");
+        radioButtons.getChildren().addAll(radioClear, radioBreakfast);
         radioButtons.setSpacing(50);
         radioButtons.setAlignment(Pos.CENTER);
 
-        rood.getChildren().addAll(buttonsScene, roodText, hBox, radioButtons, separator2, buttons);
-        stage.setScene(scene1);
+        root.getChildren().addAll(buttonsScene, textFields, dateFields, radioButtons, buttons);
+        stage.setScene(scene);
         stage.show();
 
         //event handling
         buttonScene.setOnAction(e -> new Scene2().scene2(stage));//switch to scene2
-        fieldDataClearing(buttonClear,fieldEmail, fieldName, datePicker1, datePicker2, radioBreakfasr, radioClear);//field data clearing
+        fieldDataClearing(buttonClear,fieldEmail, fieldName, datePicker1, datePicker2, radioBreakfast, radioClear);//field data clearing
     }
 
     private void fieldDataClearing(
             Button buttonClear, TextField fieldEmail, TextField fieldName,
             DatePicker datePicker1, DatePicker datePicker2,
-            RadioButton radioBreakfasr, RadioButton radioClear)
+            RadioButton radioBreakfast, RadioButton radioClear)
     {
         buttonClear.setOnAction(e ->
         {
@@ -109,7 +107,7 @@ class Scene1 {
             datePicker1.getEditor().clear();
             datePicker2.getEditor().clear();
             radioClear.setSelected(false);
-            radioBreakfasr.setSelected(false);
+            radioBreakfast.setSelected(false);
         });
 
     }
--- src/main/java/scenes/Scene2.java
+++ src/main/java/scenes/Scene2.java
@@ -5,17 +5,23 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 class Scene2 {
 
     void scene2(Stage stage) {
-        VBox rood = new VBox();
-        rood.setSpacing(10);
+        VBox root = new VBox();
+        root.setSpacing(10);
         Label label = new Label("Scene 2");
-        Scene scene2 = new Scene(rood, 800, 600);
+        Scene scene2 = new Scene(root, 800, 600);
+
+        //set style
+        BackgroundImage myBI= new BackgroundImage(new Image("8M5A7719 edited.jpg",800,600,false,true),
+                BackgroundRepeat.ROUND, BackgroundRepeat.ROUND, BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
+        root.setBackground(new Background(myBI));
 
         //set buttons switcher scenes
         HBox buttonsScene = new HBox();
@@ -23,20 +29,20 @@ class Scene2 {
         buttonsScene.getChildren().addAll(buttonScene);
 
         //set text fields
-        VBox rood1 = new VBox();
-        rood1.setSpacing(10);
-        rood1.setAlignment(Pos.CENTER);
+        VBox textFields = new VBox();
+        textFields.setSpacing(10);
+        textFields.setAlignment(Pos.CENTER);
         TextField fieldName = new TextField();
         fieldName.setMaxWidth(500);
         fieldName.setPromptText("Enter name");
         TextField fieldEmail = new TextField();
         fieldEmail.setMaxWidth(500);
         fieldEmail.setPromptText("Enter email");
-        rood1.getChildren().addAll(label, fieldName, fieldEmail);
+        textFields.getChildren().addAll(label, fieldName, fieldEmail);
 
         buttonScene.setOnAction(e -> new Scene1().scene1(stage));
 
-        rood.getChildren().addAll(buttonsScene, rood1);
+        root.getChildren().addAll(buttonsScene, textFields);
         stage.setScene(scene2);
     }
 }
