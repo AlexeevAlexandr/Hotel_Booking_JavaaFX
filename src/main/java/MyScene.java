62d0432841cf10372ff6ca6ca659a8f29985f7f6
alexandr <fart82@ukr.net>
2018-11-26 11:55:16 +0200

    a

--- src/main/java/MyScene.java
+++ src/main/java/MyScene.java
@@ -4,6 +4,7 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -23,40 +24,66 @@ public class MyScene extends Application {
         stage.setTitle("Hotel Booking");
         VBox rood = new VBox();
         rood.setAlignment(Pos.CENTER);
+        Label label = new Label();
+        Scene scene = new Scene(rood, 800, 600);
+        rood.setSpacing(10);
 
-        TextField fieldName = new TextField();
-        fieldName.setMaxWidth(300);
-        fieldName.setPromptText("Enter name");
-        TextField fieldEmail = new TextField();
-        fieldEmail.setMaxWidth(300);
-        fieldEmail.setPromptText("Enter email");
-
+        //reading from file
         ArrayList<String> arrayList = new ArrayList<>();
-
         try (Scanner scanner = new Scanner(new FileReader("src/main/resources/list.txt"))){
             while (scanner.hasNext()) {
                 arrayList.add(scanner.nextLine());
             }
-
         }catch (IOException e){e.printStackTrace();}
 
+        //view on window
         ListView<String> list = new ListView<>();
         ObservableList<String> items = FXCollections.observableArrayList (arrayList);
         list.setItems(items);
+        list.setMaxWidth(500);
+        list.setMaxHeight(200);
 
-        list.setMaxWidth(300);
-        list.setMaxHeight(70);
+        //set text fields
+        TextField fieldName = new TextField();
+        fieldName.setMaxWidth(500);
+        fieldName.setPromptText("Enter name");
+        TextField fieldEmail = new TextField();
+        fieldEmail.setMaxWidth(500);
+        fieldEmail.setPromptText("Enter email");
 
-        Button button = new Button("set");
+        //set date fields
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(100);
+        DatePicker datePicker1 = new DatePicker();
+        datePicker1.setPromptText("Enter date from");
+        DatePicker datePicker2 = new DatePicker();
+        datePicker2.setPromptText("Enter date till");
+        hBox.getChildren().addAll(datePicker1, datePicker2);
 
-        Label label = new Label();
-        Scene scene = new Scene(rood, 800, 600);
+        //set buttons
+        HBox buttons = new HBox();
+        buttons.setAlignment(Pos.CENTER);
+        buttons.setSpacing(20);
+        Button button1 = new Button("set");
+        Button button2 = new Button("clear");
+        buttons.getChildren().addAll(button1, button2);
 
-        Separator separator = new Separator();
-        separator.setMaxWidth(300);
+        //set radio buttons
+        RadioButton radioSweep = new RadioButton("Add clearing 1$");
+        RadioButton radioSin = new RadioButton("Add breakfast 2$");
+        HBox radioButtons = new HBox();
+        radioButtons.getChildren().addAll(radioSweep, radioSin);
+        radioButtons.setSpacing(50);
+        radioButtons.setAlignment(Pos.CENTER);
 
-        rood.getChildren().addAll(label, list, fieldName, fieldEmail, separator, button);
+        //set separators lines
+        Separator separator1 = new Separator();
+        separator1.setMaxWidth(500);
+        Separator separator2 = new Separator();
+        separator2.setMaxWidth(500);
 
+        rood.getChildren().addAll(label, list, separator1, fieldName, fieldEmail, hBox, radioButtons, separator2, buttons);
         stage.setScene(scene);
         stage.show();
     }
