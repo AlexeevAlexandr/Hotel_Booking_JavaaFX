4e16a53ebb117791edcfaf8a5ffd38ae273cefd8
alexandr <fart82@ukr.net>
2018-11-27 23:23:07 +0200

    a

--- src/main/java/scenes/Main.java
+++ src/main/java/scenes/Main.java
@@ -3,14 +3,15 @@ package scenes;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class MainScene extends Application {
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage stage) {
         new Scene1().scene1(stage);
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
